4904f5cbf38b4990e63adf1c37ab35489f0cef33
iskendernuori <isknuori@gmail.com>
2020-07-08 02:31:54 +0300

    dorduncu local repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,7 +6,7 @@ public class FirstClass {
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from locak repository");
 		System.out.println("third commit from locak repository");
-		
+		System.out.println("fourth commit from local repository");
 		
 
 	}
